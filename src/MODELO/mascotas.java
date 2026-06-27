0721e95eb5c00be47518ef156dfbff9b313e19f1
Jamesostin-15-SY <jamesostin15@gmail.com>
2026-06-27 11:11:19 -0500

    CLASES

---
+++ src/MODELO/mascotas.java
@@ -0,0 +1,10 @@
+package MODELO;
+import java.time.LocalDate;
+public class mascotas {
+    int id_mascota;
+    String fk_dni_cliente;
+    String nombre_mascota;
+    LocalDate fecha_nacimiento;
+    String fk_id_especie;
+    String raza;
+}

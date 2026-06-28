693a5357a7dbcefe66eccdfa45cc8f24baab65b6
REYJAZ <reynerjesuszegarra28@gmail.com>
2026-06-28 12:09:36 -0500

    commit 02

---
+++ src/DAO/Conexion.java
@@ -0,0 +1,20 @@
+package DAO;
+
+import java.sql.*;
+import Procesos.Mensajes;
+
+public class Conexion implements Parametros {
+
+    private Connection con;
+    
+    public Conexion() {
+        try {
+            Class.forName(DRIVER);
+            con = DriverManager.getConnection(URL, USUARIO, CLAVE);            
+        } catch (Exception ex) {
+            Mensajes.M1("ERROR NO SE PUEDE CONECTAR A LA BASE DE DATOS VETERINARIA..." + ex);
+        }  
+    }
+    public Connection getCon() {return con;}
+    public void setCon(Connection con) {this.con = con;}  
+}

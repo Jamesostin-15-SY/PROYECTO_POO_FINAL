a7ab6cd0d3aa232c4f526f379be364009d337407
Jamesostin-15-SY <jamesostin15@gmail.com>
2026-06-28 11:40:16 -0500

    CLASES

---
+++ src/MODELO/Clientes.java
@@ -0,0 +1,11 @@
+package MODELO;
+
+public class Clientes {
+    String dni_cliente;
+    String primer_nombre;
+    String segundo_nombre;
+    String apellido_paterno;
+    String apellido_materno;
+    String direccion;
+    String telefono;
+}
